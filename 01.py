20dd0f70ae1fd51ae09dd22858b1163edf8199a8
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-04-17 20:51:23 -0400

    commit

---
+++ 01.py
@@ -0,0 +1,10 @@
+books = dict()
+books['Gone With The Wind'] = 'Margaret Mitchell'
+books['To Kill A Mockingbird'] = 'Harper Lee'
+books['It'] = 'Stephen King'
+books['The Color Purple'] = 'Alice Walker'
+print(books)
+authors = list()
+for i in books.values():
+    authors.append(i)
+print(authors)
